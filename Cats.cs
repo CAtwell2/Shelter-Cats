0fd47d7568b396883beaaafe62443ff36c54c4bb
Caden Atwell <99625358+CAtwell2@users.noreply.github.com>
2022-08-24 21:28:50 -0400

    Add files via upload

---
+++ Cats.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number of female cats: ");
+int FemaleCats = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of male cats: ");
+int MaleCats = Convert.ToInt32(Console.ReadLine());
+int TotalCats = FemaleCats + MaleCats;
+float FemalePercentage = (FemaleCats /(float) TotalCats) * 100;
+float MalePercentage = (MaleCats /(float) TotalCats) * 100;
+Console.WriteLine("Female Cats Percentage:"+FemalePercentage);
+Console.WriteLine("Male cats percentage:"+MalePercentage);
+        
+    
